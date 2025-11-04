f6d06591dd388810947b8815d32ff7eda67660df
mati <mferrerovilas@frba.utn.edu.ar>
2025-11-04 13:45:36 -0300

    saco re-renderes innecesarios

--- src/components/movements/FiltrosMovement.tsx
+++ src/components/movements/FiltrosMovement.tsx
@@ -1,35 +1,41 @@
 import { Card, Col, Input, Row, Select } from "antd";
-import { TypeEnum } from "../../enums/TypeExpense";
+import { useCallback, useEffect, useState } from "react";
+import type { MovementFilters } from "../../routes/movement";
 import { BankEnum } from "../../enums/BankEnum";
+import { TypeEnum } from "../../enums/TypeExpense";
 import { CurrencyEnum } from "../../enums/CurrencyEnum";
 import { useCategory } from "../../apis/hooks/useCategory";
-import { useCallback } from "react";
-import type { MovementFilters } from "../../routes/movement";
+
 const { Option } = Select;
 
 const capitalize = (str: string) =>
   str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
-interface FiltersMovementProps {
-  filters: MovementFilters;
-  onChange: (
-    key: keyof MovementFilters,
-    value: string | boolean | null | BankEnum[] | TypeEnum[] | string[]
-  ) => void;
+interface Props {
+  onFiltersChange: (filters: MovementFilters) => void;
+  initialFilters: MovementFilters;
 }
-export default function FiltrosMovement({
-  filters,
-  onChange,
-}: FiltersMovementProps) {
-  const { data: categories = [] } = useCategory();
 
-  const handleFilterChange = useCallback(
+export default function FiltrosMovement({
+  onFiltersChange,
+  initialFilters,
+}: Props) {
+  const { data: categories = [] } = useCategory();
+  const [filters, setFilters] = useState<MovementFilters>(initialFilters);
+
+  const handleChange = useCallback(
     (
       key: keyof MovementFilters,
       value: string | boolean | null | BankEnum[] | TypeEnum[] | string[]
-    ) => onChange(key, value),
-    [onChange]
+    ) => {
+      setFilters((prev) => ({ ...prev, [key]: value }));
+    },
+    []
   );
+
+  useEffect(() => {
+    onFiltersChange(filters);
+  }, [filters, onFiltersChange]);
 
   return (
     <Card title="Filtros" style={{ marginBottom: 16 }}>
@@ -38,7 +44,7 @@ export default function FiltrosMovement({
           <Input
             placeholder="Buscar..."
             value={filters.description ?? ""}
-            onChange={(e) => handleFilterChange("description", e.target.value)}
+            onChange={(e) => handleChange("description", e.target.value)}
             style={{ width: 200 }}
           />
         </Col>
@@ -46,7 +52,7 @@ export default function FiltrosMovement({
           <Select
             mode="multiple"
             value={filters.type}
-            onChange={(val) => handleFilterChange("type", val as TypeEnum[])}
+            onChange={(val) => handleChange("type", val as TypeEnum[])}
             style={{ width: 200 }}
             placeholder="Todos los Tipos"
             allowClear
@@ -62,7 +68,7 @@ export default function FiltrosMovement({
           <Select
             mode="multiple"
             value={filters.bank}
-            onChange={(val) => handleFilterChange("bank", val as BankEnum[])}
+            onChange={(val) => handleChange("bank", val as BankEnum[])}
             style={{ width: 200 }}
             placeholder="Todos los bancos"
             allowClear
@@ -78,9 +84,7 @@ export default function FiltrosMovement({
           <Select
             mode="multiple"
             value={filters.currency}
-            onChange={(val) =>
-              handleFilterChange("currency", val as CurrencyEnum[])
-            }
+            onChange={(val) => handleChange("currency", val as CurrencyEnum[])}
             style={{ width: 200 }}
             placeholder="Todas las monedas"
             allowClear
@@ -96,11 +100,9 @@ export default function FiltrosMovement({
           <Select
             mode="multiple"
             value={filters.categories}
-            onChange={(val) =>
-              handleFilterChange("categories", val as string[])
-            }
+            onChange={(val) => handleChange("categories", val as string[])}
             style={{ width: 200 }}
-            placeholder="Todos los Categorías"
+            placeholder="Todas las Categorías"
             allowClear
           >
             {categories.map((cat) => (
--- src/routes/movement.tsx
+++ src/routes/movement.tsx
@@ -1,17 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Card, Segmented } from "antd";
-import { useCallback, useState } from "react";
-import { BankEnum } from "../enums/BankEnum";
-import { TypeEnum } from "../enums/TypeExpense";
-import MovementTable from "../components/movements/tables/MovementTable";
+import { useCallback, useRef, useState } from "react";
 import { HistoryOutlined, RiseOutlined } from "@ant-design/icons";
-import { CurrencyEnum } from "../enums/CurrencyEnum";
+import MovementTable from "../components/movements/tables/MovementTable";
 import AddEditMovementModal from "../components/modals/movements/AddEditMovementModal";
 import FiltrosMovement from "../components/movements/FiltrosMovement";
+import { BankEnum } from "../enums/BankEnum";
+import { TypeEnum } from "../enums/TypeExpense";
+import { CurrencyEnum } from "../enums/CurrencyEnum";
 
 export const Route = createFileRoute("/movement")({
   component: RouteComponent,
 });
+
 export type MovementFilters = {
   description: string | null;
   type: TypeEnum[];
@@ -20,6 +21,7 @@ export type MovementFilters = {
   isLive: boolean;
   currency: CurrencyEnum[];
 };
+
 function RouteComponent() {
   const [filters, setFilters] = useState<MovementFilters>({
     currency: [],
@@ -30,15 +32,17 @@ function RouteComponent() {
     isLive: true,
   });
 
-  const handleFilterChange = useCallback(
-    (
-      key: keyof MovementFilters,
-      value: string | boolean | null | BankEnum[] | TypeEnum[] | string[]
-    ) => {
-      setFilters((prev) => ({ ...prev, [key]: value }));
-    },
-    []
-  );
+  const filtersRef = useRef(filters);
+
+  const handleFiltersChange = useCallback((newFilters: MovementFilters) => {
+    filtersRef.current = newFilters;
+    setFilters(newFilters);
+  }, []);
+
+  const handleLiveChange = useCallback((val: boolean) => {
+    setFilters((prev) => ({ ...prev, isLive: val }));
+  }, []);
+
   return (
     <div style={{ paddingInline: 100, marginLeft: 80, marginRight: 80 }}>
       <div
@@ -70,14 +74,18 @@ function RouteComponent() {
             },
           ]}
           value={filters.isLive}
-          onChange={(val) => handleFilterChange("isLive", val)}
+          onChange={handleLiveChange}
           size="large"
           shape="round"
         />
 
         <AddEditMovementModal />
       </div>
-      <FiltrosMovement filters={filters} onChange={handleFilterChange} />
+
+      <FiltrosMovement
+        initialFilters={filters}
+        onFiltersChange={handleFiltersChange}
+      />
 
       <Card title="Movimientos" style={{ marginBottom: 16, padding: 0 }}>
         <MovementTable filters={filters} />

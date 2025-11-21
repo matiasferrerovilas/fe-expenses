94f4765f5bab989fc8fe24acece813f4c8802e5e
mati <mferrerovilas@frba.utn.edu.ar>
2025-11-21 09:55:40 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,27 @@ export default defineConfig({
   define: {
     global: "window",
   },
+  build: {
+    chunkSizeWarningLimit: 1500, // opcional, solo para limpiar warnings
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor";
+            if (id.includes("@tanstack")) return "tanstack";
+            if (id.includes("dayjs")) return "dayjs";
+            if (id.includes("antd")) return "antd"; // si usás Ant Design
+            return "vendor";
+          }
+
+          // Dividí módulos propios muy pesados (por tus reportes)
+          if (id.includes("movement")) return "movement";
+          if (id.includes("balance")) return "balance";
+
+          return undefined;
+        },
+      },
+    },
+  },
 });
